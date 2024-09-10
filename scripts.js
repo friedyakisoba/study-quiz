220ead6d980c25bb6a6a2adb1dcd1cbb56c58384
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-10 11:30:12 -1000

    redid the whole js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,28 @@
-let question = {};
-let questionKeys = [];
-let currentQuestion = null;
+let currentQuestions = [];
+let userAnswers = [];
+
+async function fetchQuestions() {
+    try {
+        // Fetch the JSON File 
+        const response = await ('questions.json');
+        // Parse the JSON File
+        const questions = await response.json();
+        return question;
+    } catch (error){
+        // Error handling
+        console.error('Error fetching questions:', error);
+        
+    }
+}
+
+// Random Question Generator (5 for debugging | 10 for final)
+function getRandomQuestions(questionSet) {
+    // shuffles the question
+    const shuffled =questionSet.sort(()=> 0.5 - Math.random());
+    // returns the first five questions
+    return shuffled.slice(0, 5);
+}
+
 
 
 const questionElement = document.getElementById('question');
@@ -19,10 +41,8 @@ function askRandomQuestion() {
 button.addEventListener('click', askRandomQuestion);
 
 
-// const questions = [
-//     "What is the capital of your country?",
-//     "Who is the current president?",
-//     "What year did your country gain independence?",
-//     "What is the national anthem of your country?",
-//     "What are the colors of your country's flag?"
-// ];
+
+
+
+
+

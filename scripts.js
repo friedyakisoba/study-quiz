6a493f84db419ffb8574031e434b7815365cb6f5
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-10 09:26:36 -1000

    changed the html form to multiple choice instead of free form answers

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
+let question = {};
+let questionKeys = [];
+let currentQuestion = null;
+
+
 const questionElement = document.getElementById('question');
 const button = document.getElementById('new');
 
-const questions = [
-    "What is the capital of your country?",
-    "Who is the current president?",
-    "What year did your country gain independence?",
-    "What is the national anthem of your country?",
-    "What are the colors of your country's flag?"
-];
 
 function getRandomIndex() {
     return Math.floor(Math.random() * questions.length);
@@ -19,3 +17,12 @@ function askRandomQuestion() {
 }
 
 button.addEventListener('click', askRandomQuestion);
+
+
+// const questions = [
+//     "What is the capital of your country?",
+//     "Who is the current president?",
+//     "What year did your country gain independence?",
+//     "What is the national anthem of your country?",
+//     "What are the colors of your country's flag?"
+// ];

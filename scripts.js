1387e4f3489c1c71e622e8dae6670ce25e07d079
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-09-10 12:39:42 -1000

    started on creating a function generating questions and answers

--- scripts.js
+++ scripts.js
@@ -8,31 +8,58 @@ async function fetchQuestions() {
         // Parse the JSON File
         const questions = await response.json();
         return question;
-    } catch (error){
+    } catch (error) {
         // Error handling
         console.error('Error fetching questions:', error);
-        
+
     }
 }
 
 // Random Question Generator (5 for debugging | 10 for final)
 function getRandomQuestions(questionSet) {
     // shuffles the question
-    const shuffled =questionSet.sort(()=> 0.5 - Math.random());
+    const shuffled = questionSet.sort(() => 0.5 - Math.random());
     // returns the first five questions
     return shuffled.slice(0, 5);
 }
 
-function showQuestions () {
+function showQuestions() {
     const questionElement = document.getElementById('question');
     const choicesElement = document.getElementById('choices');
     questionElement.textContent = "Citizenship Quiz";
 
+
+
     // clear any previous questions and choices
     choicesElement.innerHTML = '';
     userAnswers = [];
+    currentQuestions.forEach((q, index) => {
+        const li = document.createElement('li');
+        const questionText = document.createElement('p');
+        questionText.textContent = `${index + 1}. ${q.question}`;
+        li.appendChild(questionText);
+
+        q.choices.forEach(() => {
+            const choiceItem = document.createElement('div');
+            const input = document.createElement('input');
+            input.type = 'radio';
+            input.name = `question-${index}`;
+            input.value = choice;
+            input.classList.add('answers');
+
+
+            const label = document.createElement('label');
+            label.textContent = choice;
+
+        });
+    });
+
 
 }
+
+
+
+
 
 
 function gradeQuiz() {
@@ -49,10 +76,11 @@ function gradeQuiz() {
 
         if (userAnswer === correctAnswer) {
             correctCount++
-        }   
+        }
     });
 
-   feedbackElement.textContent `'You got ${correctCount} out of ${correctQuestions.length} correct. `; 
+    // returns how much you got correct 
+    feedbackElement.textContent`'You got ${correctCount} out of ${correctQuestions.length} correct. `;
 }
 
 
